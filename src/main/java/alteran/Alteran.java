0e0a9716d13ee74f0881d7c649a484afd382e631
xnerhu <xnerhu@gmail.com>
2021-05-22 12:58:11 +0200

    refactor: remove forge boilerplate

--- src/main/java/alteran/Alteran.java
+++ src/main/java/alteran/Alteran.java
@@ -1,9 +1,11 @@
-package com.example.examplemod;
+package alteran;
 
+import alteran.common.AlteranCommon;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -16,71 +18,18 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.minecraft.util.RegistryKey;
-import net.minecraft.util.registry.Registry;
-
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("alteran")
+@Mod(AlteranCommon.MOD_ID)
 public class Alteran {
-  // Directly reference a log4j logger.
-  private static final Logger LOGGER = LogManager.getLogger();
-
   public Alteran() {
-//    Registry.register
-    // Register the setup method for modloading
-    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-    // Register the enqueueIMC method for modloading
-    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-    // Register the processIMC method for modloading
-    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-    // Register the doClientStuff method for modloading
-    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+    IEventBus forgeBus = MinecraftForge.EVENT_BUS;
 
-    // Register ourselves for server and other game events we are interested in
-    MinecraftForge.EVENT_BUS.register(this);
+    modBus.addListener(this::onCommonSetup);
   }
 
-  private void setup(final FMLCommonSetupEvent event) {
-    // some preinit code
-    LOGGER.info("HELLO FROM PREINIT");
-    LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-  }
+  public void onCommonSetup(FMLCommonSetupEvent e) {
 
-  private void doClientStuff(final FMLClientSetupEvent event) {
-    // do something that can only be done on the client
-    LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
-  }
-
-  private void enqueueIMC(final InterModEnqueueEvent event) {
-    // some example code to dispatch IMC to another mod
-    InterModComms.sendTo("examplemod", "helloworld", () -> {
-      LOGGER.info("Hello world from the MDK");
-      return "Hello world";
-    });
-  }
-
-  private void processIMC(final InterModProcessEvent event) {
-    // some example code to receive and process InterModComms from other mods
-    LOGGER.info("Got IMC {}", event.getIMCStream().map(m -> m.getMessageSupplier().get()).collect(Collectors.toList()));
-  }
-
-  // You can use SubscribeEvent and let the Event Bus discover methods to call
-  @SubscribeEvent
-  public void onServerStarting(FMLServerStartingEvent event) {
-    // do something when the server starts
-    LOGGER.info("HELLO from server starting");
-  }
-
-  // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-  // Event bus for receiving Registry Events)
-  @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-  public static class RegistryEvents {
-    @SubscribeEvent
-    public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-      // register a new block here
-      LOGGER.info("HELLO from Register Block");
-    }
   }
 }
---
+++ src/main/java/alteran/common/AlteranCommon.java
@@ -0,0 +1,12 @@
+package alteran.common;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AlteranCommon {
+  public static final String MOD_ID = "alteran";
+
+  public static final String MOD_NAME = "Alteran";
+
+  public static final Logger LOGGER = LogManager.getLogger();
+}
